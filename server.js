f3c5d43d4ef9e8d0b983a5eec090cd0eb6c7f41e
huangsen0912 <1417988198@qq.com>
2015-09-06 14:46:51 +0800

    server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+
+var server=http.createServer(function(req,res){
+
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('Hello world\n');
+
+
+});
+
+server.listen(8080,"localhost");
+console.log("server running at http://127.0.0.1:8080/");
+
