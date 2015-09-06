d3353cadf0e31115b7b95bd4d0a17330c99a80e9
huangsen0912 <1417988198@qq.com>
2015-09-06 17:38:29 +0800

    异步执行解决问题方案

--- async.js
+++ async.js
@@ -9,15 +9,16 @@ function printIt(){
 	console.log(c);
 }
 
-function plus(){
+function plus(callback){
 	setTimeout(function(){
 		c+=1;
+		callback();
 	},1000);
 
 }
 
-plus();
-printIt();
+plus(printIt);
+//printIt();
 
 
 
